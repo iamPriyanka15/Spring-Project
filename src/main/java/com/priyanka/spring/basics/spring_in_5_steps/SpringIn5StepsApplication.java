fbd8c7e0a22233e99ad017f5e7214d210110333a
iamPriyanka15 <161258029+iamPriyanka15@users.noreply.github.com>
2024-07-19 14:38:55 -0600

    setup github repository with spring project

---
+++ src/main/java/com/priyanka/spring/basics/spring_in_5_steps/SpringIn5StepsApplication.java
@@ -0,0 +1,13 @@
+package com.priyanka.spring.basics.spring_in_5_steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIn5StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIn5StepsApplication.class, args);
+	}
+
+}
