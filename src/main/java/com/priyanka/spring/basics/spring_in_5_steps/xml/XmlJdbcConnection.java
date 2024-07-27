c92f4703a8b895e992fd9d8846470f3d883a12dd
iamPriyanka15 <161258029+iamPriyanka15@users.noreply.github.com>
2024-07-27 15:06:20 -0600

    Defining Spring Application Context using XML

--- src/main/java/com/priyanka/spring/basics/spring_in_5_steps/xml/XmlJdbcConnection.java
+++ src/main/java/com/priyanka/spring/basics/spring_in_5_steps/xml/XmlJdbcConnection.java
@@ -6,11 +6,11 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
-@Component
-@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
+
 public class XmlJdbcConnection {
 
     public XmlJdbcConnection(){
+
         System.out.println("Jdbc Connection");
     }
 }
